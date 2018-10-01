cbb369ad19eb67dadcbc27e33dd873f56436eccb
n2m2912 <mannguyen281129@gmail.com>
2018-10-01 22:15:21 +0700

    add script

---
+++ README.md
@@ -0,0 +1,1 @@
+Linux initialization script
---
+++ linux-init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "================update=================="
+apt-get update
+echo "================upgrade====================="
+apt-get upgrade -y
+echo "================dist-upgrade================"
+apt-get dist-upgrade -y
+echo "================clean up===================="
+apt-get autoremove -y
+apt-get autoclean -y
+echo "==============linux headers========"
+apt-get install -y linux-headers-$(uname -r)
+echo "==============apt transport https========="
+apt-get install -y apt-transport-https 
+echo "=============ca certificates========="
+apt-get install -y ca-certificates 
+echo "=============wget==============="
+apt-get install -y wget 
+echo "=============software properties common=========="
+apt-get install -y software-properties-common
+echo "=============htop==============="
+apt-get install -y htop
+echo "=============speedtest-cli======="
+apt-get install -y speedtest-cli
+echo "=============ranger=============="
+apt-get install -y ranger
+echo "=============ssh================="
+apt-get install -y ssh
+echo "=============cifs utils=========="
+apt-get install -y cifs-utils
+echo "=============network tool========"
+apt-get install -y net-tools
+echo "=============bleach bit========="
+apt-get install -y bleachbit
