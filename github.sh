132c84839dc2235310c61e125aee33e69da450a5
root <root@localhost.localdomain>
2018-10-12 10:19:29 +0700

    Add github link

---
+++ github.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir git
+cd git/
+echo "============airgeddon=========="
+git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git
+echo "============cupp==============="
+git clone https://github.com/Mebus/cupp.git
+echo "=========HT-WPS Breaker========"
+git clone https://github.com/SilentGhostX/HT-WPS-Breaker.git
+echo "=========krackattacks-scripts==="
+git clone https://github.com/vanhoefm/krackattacks-scripts.git
